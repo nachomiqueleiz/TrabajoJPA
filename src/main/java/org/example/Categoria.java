9da83d6012d6ff5816c9c911cf3a10f0481a675a
Nacho <nachi.miq@gmail.com>
2024-09-04 13:05:58 -0300

    Commit 1

---
+++ src/main/java/org/example/Categoria.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+
+@Entity
+@Table(name = "Categoria")
+public class Categoria implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String denominacion;
+    @ManyToMany(mappedBy = "categorias")
+    @Builder.Default
+    private List<Articulo> articulos = new ArrayList<Articulo>();
+}
---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,35 @@
+package org.example;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long dni;
+    private String nombre;
+    private String apellido;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+
+    @OneToMany(mappedBy = "cliente")
+    @Builder.Default
+    private List<Factura> facturas = new ArrayList<Factura>();
+}
